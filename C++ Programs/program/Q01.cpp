0f98711fea443a59d565d64bd064c17aea47cb48
Anil Kumar <54653301+Anilbishnoi29@users.noreply.github.com>
2023-01-23 07:20:39 +0530

    Fb

--- C++ Programs/program/Q01.cpp
+++ C++ Programs/program/Q01.cpp
@@ -23,4 +23,56 @@ int main() {
 	AverageMarks MyAvgMarks( marks1, marks2, marks3 );
 	cout << "Average marks : " << MyAvgMarks.avgMarks() << endl;
 	return 0;
-}
+
+
+
+
+#############₹₹##₹#₹
+from playwright import sync_playwright
+
+with sync_playwright() as playwright:
+
+    browser = playwright.chromium.launch()
+
+    context = browser.new_context()
+
+    page = context.new_page()
+
+    # Log in to Facebook
+
+    page.goto("https://www.facebook.com/")
+
+    page.type("#email", "YOUR_EMAIL")
+
+    page.type("#pass", "YOUR_PASSWORD")
+
+    page.click("#loginbutton")
+
+    # Go to your Facebook profile
+
+    page.goto("https://www.facebook.com/YOUR_USERNAME/")
+
+    # Get all post IDs
+
+    post_ids = []
+
+    while True:
+
+        post_elements = page.query_selector_all("._1xnd")
+
+        for post_element in post_elements:
+
+            post_id = post_element.get_attribute("id")
+
+            post_ids.append(post_id)
+
+        if not page.query_selector("._4khu"):
+
+            break
+
+        page.click("._4khu")
+
+    print(post_ids)
+
+    browser.close()
+
